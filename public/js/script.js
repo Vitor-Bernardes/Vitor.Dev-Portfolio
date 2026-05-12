41401b68be66bc0eced679e103daf3eb6594bc8c
Vks9 <vitorsbc589@gmail.com>
2026-05-12 18:27:12 -0300

    refactor: add project cards

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,42 @@ menuBtn.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
 	menuMobile.classList.add("translate-x-full");
 });
+
+//Cards Porjects
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
+const closeModal = document.getElementById("closeModal");
+
+// abre modal ao clicar no card
+document.querySelectorAll("[data-modal]").forEach((btn) => {
+	btn.addEventListener("click", () => {
+		modal.classList.remove("hidden");
+		modal.classList.add("flex");
+
+		// animação suave
+		setTimeout(() => {
+			modalContent.classList.remove("scale-95", "opacity-0");
+		}, 10);
+	});
+});
+
+// fechar pelo botão
+closeModal.addEventListener("click", () => {
+	closeModalFn();
+});
+
+// fechar clicando fora
+modal.addEventListener("click", (e) => {
+	if (e.target === modal) {
+		closeModalFn();
+	}
+});
+
+function closeModalFn() {
+	modalContent.classList.add("scale-95", "opacity-0");
+
+	setTimeout(() => {
+		modal.classList.add("hidden");
+		modal.classList.remove("flex");
+	}, 200);
+}
